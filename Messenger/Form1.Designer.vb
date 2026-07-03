8cd8a02234d9a8ebe7e31b6716937ee93ab5e580
TheDoggyBrad Software Labs <94173621+thedoggybrad@users.noreply.github.com>
2026-07-03 10:55:58 +0800

    Add files via upload

---
+++ Messenger/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'WebView21
+        '
+        Me.WebView21.AllowExternalDrop = True
+        Me.WebView21.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebView21.CreationProperties = Nothing
+        Me.WebView21.DefaultBackgroundColor = System.Drawing.Color.White
+        Me.WebView21.Location = New System.Drawing.Point(0, 0)
+        Me.WebView21.Name = "WebView21"
+        Me.WebView21.Size = New System.Drawing.Size(1195, 807)
+        Me.WebView21.Source = New System.Uri("https://messenger.com", System.UriKind.Absolute)
+        Me.WebView21.TabIndex = 0
+        Me.WebView21.ZoomFactor = 1.0R
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(1193, 806)
+        Me.Controls.Add(Me.WebView21)
+        Me.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Messenger"
+        CType(Me.WebView21, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
+End Class
---
+++ Messenger/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports Microsoft.Web.WebView2.Core
+Imports Microsoft.Web.WebView2
+Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LogMsg("MS Edge Version: " + CoreWebView2Environment.GetAvailableBrowserVersionString())
+    End Sub
+
+    Private Sub LogMsg(msg As String, Optional addTimestamp As Boolean = True)
+        'ToDo: add desired code
+
+        If addTimestamp Then
+            msg = String.Format("{0} - {1}", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff"), msg)
+        End If
+
+        Debug.WriteLine(msg)
+    End Sub
+    Private Sub WebsiteNavigate(ByVal dest As String)
+        If WebView21 IsNot Nothing AndAlso Not String.IsNullOrWhiteSpace(dest) Then
+
+            'URL must start with one of the specified strings
+            'if Not, pre-pend with "https://"
+            If Not dest = "about:blank" AndAlso
+                   Not dest.StartsWith("http://") AndAlso
+                   Not dest.StartsWith("https://") AndAlso
+                   Not System.Text.RegularExpressions.Regex.IsMatch(dest, "^([A-Z]|[a-z]):") Then
+
+                'set value
+                dest = "https://" & dest
+            End If
+
+            'option 1
+            WebView21.Source = New Uri(dest, UriKind.Absolute)
+
+            'option 2
+            'WebView21.CoreWebView2.Navigate(dest)
+        End If
+    End Sub
+
+    Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        LogMsg("WebView21_CoreWebView2InitializationCompleted")
+        LogMsg("UserDataFolder: " & WebView21.CoreWebView2.Environment.UserDataFolder.ToString())
+    End Sub
+
+    Private Sub WebView21_NavigationCompleted(sender As Object, e As Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs) Handles WebView21.NavigationCompleted
+        LogMsg("WebView21_NavigationCompleted")
+    End Sub
+
+    Private Sub WebView21_WebMessageReceived(sender As Object, e As Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs) Handles WebView21.WebMessageReceived
+        LogMsg("WebView21_WebMessageReceived")
+    End Sub
+
+    Private Sub WebView21_Click(sender As Object, e As EventArgs) Handles WebView21.Click
+
+    End Sub
+End Class
